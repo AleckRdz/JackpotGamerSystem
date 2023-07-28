890eba4176b4580afd5a2b881a4cacfa209fe015
Aleck Rdz <rdzaleck@gmail.com>
2023-07-28 13:37:28 -0600

    logo and header modify

--- src/masterpage/header.php
+++ src/masterpage/header.php
@@ -33,25 +33,25 @@
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
                         <a href="index.php" class="logo">
-                            <img src="assets/img/logo.gif" alt="SnapX Photography Template">
+                            <img src="assets/img/logo.png" alt="LogoJackpotGamer">
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-                            <li><a href="index.php" class="active">Home</a></li>
+                            <li><a href="index.php" class="active">Inicio</a></li>
                             <li class="has-sub">
-                                <a href="javascript:void(0)">Photos &amp; Videos</a>
+                                <a href="javascript:void(0)">Rifas</a>
                                 <ul class="sub-menu">
-                                    <li><a href="contests.php">Contests</a></li>
-                                    <li><a href="contest-details.php">Single Contest</a></li>
+                                    <li><a href="contests.php">Actual</a></li>
+                                    <li><a href="contest-details.php">Pasadas</a></li>
                                 </ul>
                             </li>
-                            <li><a href="categories.php">Categories</a></li>
-                            <li><a href="users.php">Users</a></li>
+                            <li><a href="categories.php">Nosotros</a></li>
+                            <li><a href="users.php">Contacto</a></li>
                         </ul>
-                        <div class="border-button">
+                        <!-- <div class="border-button">
                             <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
-                        </div>
+                        </div> -->
                         <a class='menu-trigger'>
                             <span>Menu</span>
                         </a>
@@ -62,90 +62,3 @@
         </div>
     </header>
     <!-- ***** Header Area End ***** -->
-
-    <div id="modal" class="popupContainer" style="display:none;">
-        <div class="popupHeader">
-            <span class="header_title">Login</span>
-            <span class="modal_close"><i class="fa fa-times"></i></span>
-        </div>
-
-        <section class="popupBody">
-            <!-- Social Login -->
-            <div class="social_login">
-                <div class="">
-                    <a href="#" class="social_box fb">
-                        <span class="icon"><i class="fab fa-facebook"></i></span>
-                        <span class="icon_title">Connect with Facebook</span>
-
-                    </a>
-
-                    <a href="#" class="social_box google">
-                        <span class="icon"><i class="fab fa-google-plus"></i></span>
-                        <span class="icon_title">Connect with Google</span>
-                    </a>
-                </div>
-
-                <div class="centeredText">
-                    <span>Or use your Email address</span>
-                </div>
-
-                <div class="action_btns">
-                    <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
-                    <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
-                </div>
-            </div>
-
-            <!-- Username & Password Login form -->
-            <div class="user_login">
-                <form action="" method="post">
-                    <label>Email / Username</label>
-                    <input name="username" type="text" id="username" />
-                    <br />
-
-                    <label>Password</label>
-                    <input name="password" type="password" id="password" />
-                    <br />
-
-                    <div class="checkbox">
-                        <input id="remember" type="checkbox" />
-                        <label for="remember">Remember me on this computer</label>
-                    </div>
-
-                    <div class="action_btns">
-                        <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
-                        <div class="one_half last"><button type="submit" class="btn btn_red">Login</button></div>
-                    </div>
-                </form>
-
-                <a href="#" class="forgot_password">Forgot password?</a>
-            </div>
-
-            <!-- Register Form -->
-            <div class="user_register">
-                <form action="" method="post">
-                    <label>Username</label>
-                    <input name="username" type="text" id="username" />
-                    <br />
-
-                    <label>Email Address</label>
-                    <input name="email" type="email" id="email" />
-                    <br />
-
-                    <label>Password</label>
-                    <input name="password" type="password" id="password" />
-                    <br />
-
-                    <div class="checkbox">
-                        <input id="send_updates" type="checkbox" />
-                        <label for="send_updates">Send me occasional email updates</label>
-                    </div>
-
-                    <div class="action_btns">
-                        <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
-                        <div class="one_half last"><button type="submit" class="btn btn_red">Register</button></div>
-                    </div>
-                </form>
-            </div>
-
-        </section>
-    </div>

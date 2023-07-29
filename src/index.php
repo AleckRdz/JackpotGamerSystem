f0b996450454fd075d418cf251fc203ee3ca2f1a
Aleck Rdz <rdzaleck@gmail.com>
2023-07-28 20:05:02 -0600

    design modification

--- src/index.php
+++ src/index.php
@@ -6,15 +6,15 @@
     <div class="row">
       <div class="col-lg-10 offset-lg-1">
         <div class="header-text">
-          <h2>Enter a world of <em>Photos</em> &amp; Amazing <em>Awards</em></h2>
-          <p>SnapX Photography is a professional website template with 5 different HTML pages for maximum customizations. It is free for commercial usage. This Bootstrap v5.1.3 CSS layout is provided by TemplateMo Free CSS Templates.</p>
+          <h2>Jackpot <em>GAMER</em></h2>
+          <p>¡Bienvenidos a Jackpotgamermx!</p>
           <div class="buttons">
             <div class="big-border-button">
-              <a href="contests.html">Explore SnapX Contest</a>
+              <a href="contests.html">Comprar Boletos</a>
             </div>
-            <div class="icon-button">
+            <!-- <div class="icon-button">
               <a href="https://youtube.com/templatemo" target="_blank"><i class="fa fa-play"></i> Watch Our Video Now</a>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
--- src/masterpage/header.php
+++ src/masterpage/header.php
@@ -1,64 +1,67 @@
 <!DOCTYPE php>
 <php lang="en">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta name="author" content="templatemo">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <meta name="author" content="templatemo">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 
-    <title>Jackpot GAMER</title>
+        <title>Jackpot GAMER</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+        <link rel="icon" href="assets/img/favicon.ico" type="image/png" sizes="16x16">
 
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-snapx-photography.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+        <!-- Additional CSS Files -->
+        <link rel="stylesheet" href="assets/css/fontawesome.css">
+        <link rel="stylesheet" href="assets/css/templatemo-snapx-photography.css">
+        <link rel="stylesheet" href="assets/css/owl.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
-</head>
-<body>
+    </head>
 
-    <!-- ***** Header Area Start ***** -->
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="index.php" class="logo">
-                            <img src="assets/img/logo.png" alt="LogoJackpotGamer">
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li><a href="index.php" class="active">Inicio</a></li>
-                            <li class="has-sub">
-                                <a href="javascript:void(0)">Rifas</a>
-                                <ul class="sub-menu">
-                                    <li><a href="contests.php">Actual</a></li>
-                                    <li><a href="contest-details.php">Pasadas</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="categories.php">Nosotros</a></li>
-                            <li><a href="users.php">Contacto</a></li>
-                        </ul>
-                        <!-- <div class="border-button">
-                            <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
-                        </div> -->
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
+    <body>
+
+        <!-- ***** Header Area Start ***** -->
+        <header class="header-area header-sticky">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="main-nav">
+                            <!-- ***** Logo Start ***** -->
+                            <a href="index.php" class="logo">
+                                <img src="assets/img/logoJackpotRound.png" alt="LogoJackpotGamer">
+                            </a>
+                            <!-- ***** Logo End ***** -->
+                            <!-- ***** Menu Start ***** -->
+                            <ul class="nav">
+                                <li><a href="index.php" class="active">Inicio</a></li>
+                                <li><a href="contests.php" class="active">Preg. Frec.</a></li>
+                                <!-- <li class="has-sub">
+                                    <a href="javascript:void(0)">Rifas</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="contests.php">Actual</a></li>
+                                        <li><a href="contest-details.php">Pasadas</a></li>
+                                    </ul>
+                                </li> -->
+                                <li><a href="categories.php">Nosotros</a></li>
+                                <li><a href="users.php">Contacto</a></li>
+                            </ul>
+                            <div class="border-button">
+                                <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa-solid fa-ticket"></i></i> Boletos</a>
+                            </div>
+                            <a class='menu-trigger'>
+                                <span>Menu</span>
+                            </a>
+                            <!-- ***** Menu End ***** -->
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
-    </header>
-    <!-- ***** Header Area End ***** -->
+        </header>
+        <!-- ***** Header Area End ***** -->
